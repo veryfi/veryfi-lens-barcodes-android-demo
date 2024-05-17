96bfa6c511a379e41cab2040c44898957b94ce7d
Bernardo Restrepo <bernardorestrepo@veryfi.com>
2024-05-17 09:32:26 -0500

    LP-937: Updated Android Lens

--- app/src/main/java/com/veryfi/lens/barcodes/demo/logs/Log.kt
+++ app/src/main/java/com/veryfi/lens/barcodes/demo/logs/Log.kt
@@ -3,6 +3,6 @@ package com.veryfi.lens.barcodes.demo.logs
 import org.json.JSONObject
 
 data class Log(
-    val title: String = "",
-    val message: JSONObject = JSONObject()
+    var title: String = "",
+    var message: JSONObject = JSONObject()
 )
--- app/src/main/java/com/veryfi/lens/barcodes/demo/logs/LogsActivity.kt
+++ app/src/main/java/com/veryfi/lens/barcodes/demo/logs/LogsActivity.kt
@@ -1,89 +1,147 @@
 package com.veryfi.lens.barcodes.demo.logs
 
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lriccardo.timelineview.TimelineDecorator
 import com.veryfi.lens.VeryfiLens
 import com.veryfi.lens.VeryfiLensDelegate
 import com.veryfi.lens.barcodes.demo.R
 import com.veryfi.lens.barcodes.demo.databinding.ActivityLogsBinding
 import com.veryfi.lens.barcodes.demo.helpers.ThemeHelper
+import org.json.JSONException
 import org.json.JSONObject
 
 class LogsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogsBinding
     private var logsEventData: ArrayList<Log> = ArrayList()
+    private lateinit var adapter: LogsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLogsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ThemeHelper.setSecondaryColorToStatusBar(this)
-
-        val adapter = LogsAdapter()
+        setUpVeryfiLensDelegate()
         setUpToolBar()
-        setupLogsList(adapter)
-        setUpVeryfiLensDelegate(adapter)
+        loadData()
         VeryfiLens.showCamera()
     }
 
-    private fun setUpToolBar() = with(binding.topAppBar) {
-        setSupportActionBar(this)
-        setNavigationIcon(R.drawable.ic_vector_close_shape)
-        setNavigationOnClickListener { finish() }
+    private fun setUpToolBar() {
+        setSupportActionBar(binding.topAppBar)
+        binding.topAppBar.setNavigationIcon(R.drawable.ic_vector_close_shape)
+        binding.topAppBar.setNavigationOnClickListener { finish() }
     }
 
-    private fun setupLogsList(logsAdapter: LogsAdapter) = with(binding.timelineRv) {
-        layoutManager = LinearLayoutManager(this@LogsActivity, LinearLayoutManager.VERTICAL, false)
-        adapter = logsAdapter
-        val colorPrimary = ThemeHelper.getPrimaryColor(this@LogsActivity)
-        addItemDecoration(
-            TimelineDecorator(
-                position = TimelineDecorator.Position.Left,
-                indicatorColor = colorPrimary,
-                lineColor = colorPrimary
+    private fun loadData() {
+        binding.timelineRv.let {
+            it.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            adapter = LogsAdapter()
+            it.adapter = adapter
+
+            val colorPrimary = ThemeHelper.getPrimaryColor(this)
+            it.addItemDecoration(
+                TimelineDecorator(
+                    position = TimelineDecorator.Position.Left,
+                    indicatorColor = colorPrimary,
+                    lineColor = colorPrimary
+                )
             )
-        )
+
+            it.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    (it.layoutManager as? LinearLayoutManager)?.let {
+
+                    }
+                }
+            })
+        }
     }
 
-    private fun setUpVeryfiLensDelegate(adapter: LogsAdapter) {
+    @SuppressLint("NotifyDataSetChanged")
+    private fun showLogs(json: JSONObject) {
+        val log = Log()
+        log.message = json
+        val status = if (json.has(STATUS)) json.getString(STATUS) else ""
+        when (status) {
+            CLOSE -> log.title = resources.getString(R.string.logs_result)
+            START -> log.title = resources.getString(R.string.logs_start_uploading)
+            IN_PROGRESS -> {
+                when (json.getString(MSG)) {
+                    IMG_THUMBNAIL -> log.title = resources.getString(R.string.logs_thumbnail)
+                    IMG_ORIGINAL -> log.title =
+                        resources.getString(R.string.logs_original_image)
+
+                    PROGRESS -> log.title = resources.getString(R.string.logs_extraction)
+                }
+            }
+
+            DONE -> log.title = resources.getString(R.string.logs_result)
+            REMOVED -> {
+                log.title = resources.getString(R.string.logs_remove_cache_data)
+            }
+
+            EXCEPTION -> log.title = resources.getString(R.string.logs_exception)
+            ERROR -> log.title = resources.getString(R.string.logs_error)
+            FAILED -> log.title = resources.getString(R.string.logs_failed)
+            else -> log.title = "Other"
+        }
+
+        logsEventData.add(log)
+        adapter.addItem(log)
+        adapter.notifyDataSetChanged()
+        binding.timelineRv.scrollToPosition(adapter.itemCount - 1)
+
+        if (status.equals(CLOSE) && logsEventData.size == 0) {
+            finish()
+        }
+    }
+
+    private fun setUpVeryfiLensDelegate() {
         VeryfiLens.setDelegate(object : VeryfiLensDelegate {
-            override fun veryfiLensClose(json: JSONObject) { showLogs(json, adapter) }
-            override fun veryfiLensError(json: JSONObject) { showLogs(json, adapter) }
-            override fun veryfiLensSuccess(json: JSONObject) { showLogs(json, adapter) }
-            override fun veryfiLensUpdate(json: JSONObject) { showLogs(json, adapter) }
+            override fun veryfiLensClose(json: JSONObject) {
+                showLogs(json)
+            }
+
+            override fun veryfiLensError(json: JSONObject) {
+                showLogs(json)
+            }
+
+            override fun veryfiLensSuccess(json: JSONObject) {
+                showLogs(json)
+            }
+
+            override fun veryfiLensUpdate(json: JSONObject) {
+                showLogs(json)
+            }
         })
     }
 
-    private fun showLogs(json: JSONObject, adapter: LogsAdapter) {
-        val status = if (json.has(STATUS)) json.getString(STATUS) else ""
-        val title = when (status) {
-            START -> getString(R.string.logs_start_uploading)
-            IN_PROGRESS -> {
-                when (json.getString(MSG)) {
-                    IMG_THUMBNAIL -> getString(R.string.logs_thumbnail)
-                    IMG_ORIGINAL -> getString(R.string.logs_original_image)
-                    PROGRESS -> getString(R.string.logs_extraction)
-                    else -> ""
+    private inner class VeryfiServiceJsonReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val bundle = intent.extras
+            if (bundle != null) {
+                val json = JSONObject()
+                for (key in bundle.keySet()) {
+                    val value = bundle.get(key)
+                    if (value != null) {
+                        try {
+                            json.put(key, JSONObject.wrap(bundle.get(key)))
+                        } catch (e: JSONException) {
+                            //Handle exception here
+                        }
+                    }
                 }
+                showLogs(json)
             }
-            DONE -> getString(R.string.logs_result)
-            REMOVED -> getString(R.string.logs_remove_cache_data)
-            EXCEPTION -> getString(R.string.logs_exception)
-            ERROR -> getString(R.string.logs_error)
-            FAILED -> getString(R.string.logs_failed)
-            else -> "Other"
-        }
-
-        if (status.equals(CLOSE).not()) {
-            val log = Log(title = title, message = json)
-            logsEventData.add(log)
-            adapter.addItem(log)
-            binding.timelineRv.scrollToPosition(adapter.itemCount - 1)
-        } else {
-            if (logsEventData.size == 0) finish()
         }
     }
 
